ef33722903cff092ff767cef8d881d2804054f46
Rafael Hart <rafael.hart9@gmail.com>
2017-05-04 16:17:11 +1000

    These keys will be rotated very shortly

--- sendtoazure.py
+++ sendtoazure.py
@@ -2,17 +2,16 @@ import time
 import sys
 from azure.servicebus import ServiceBusService
 
-key_name="My Sender"
-key_value="Insert Key"
+key_name="RootManageSharedAccessKey"
+key_value="pjxZvmffk+hhDTXST7TzvShuZ+jxgRiKLc1T4yLGOGw="
 
-sbs = ServiceBusService("robotdeskeventhub-ns",
+sbs = ServiceBusService("stand-and-deliver",
                        shared_access_key_name=key_name,
                        shared_access_key_value=key_value)
 
 while(True):
     print('Sending...')
-    sbs.send_event('nameofeventhub', '{ "DeviceId": "StandingDesk",
-                   "State": "Standing or Seated" }')
+    sbs.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Standing or Seated" }')
     print('Sent...')
     time.sleep(10)
 
