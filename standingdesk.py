22deaa2fd9bb52c8ba534e9802bc7725dce7dfd4
Rafael Hart <rafael.hart9@gmail.com>
2017-05-04 17:16:23 +1000

    More cleanup

--- standingdesk.py
+++ standingdesk.py
@@ -16,20 +16,19 @@ desk_pin = 23
 
 ## Use the built-in PullUp resistor & initialize the desk switch
 io.setup(desk_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
-DESK = 1
+#DESK = 1
 
 while True:
     if io.input(desk_pin):
         # The switch is open
-        print("Desk: Standing Position. Sending to Azure...",)
+        print("Desk: Standing Position. Sending to Azure..."),
         SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Standing" }')
         print("done.")
-#        DESK = 0 # set door to its initial value
-        time.sleep(10) # wait 10 seconds before the next action
-        ## if the switch is closed and door does not equal 1
-        if (io.input(desk_pin)==False):
-            print("Desk: Seated Position. Sending to Azure...")
-            SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Seated" }')
-            print("done.")
-#            DESK = 1
-            # set door so that this loop won't act again until the switch has been opened
+
+    if (io.input(desk_pin)==False):
+        # The switch is closed
+        print("Desk: Seated Position. Sending to Azure..."),
+        SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Seated" }')
+        print("done.")
+
+    time.sleep(10) # wait 10 seconds before the next action
