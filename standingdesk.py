48d8b1e8d7fed0b6e75247a6bee0c212fdc39624
Rafael Hart <rafael.hart9@gmail.com>
2017-05-04 17:25:40 +1000

    Adding some error handling

--- standingdesk.py
+++ standingdesk.py
@@ -10,25 +10,27 @@ SBS = ServiceBusService("stand-and-deliver",
                         shared_access_key_name=key_name,
                         shared_access_key_value=key_value)
 
-## Set GPIO mode to BCM, busing the GPIO number (in this case, 23)
+# Set GPIO mode to BCM, using the GPIO number (in this case, 23)
 io.setmode(io.BCM)
 desk_pin = 23
 
-## Use the built-in PullUp resistor & initialize the desk switch
+# Use the built-in PullUp resistor & initialize the desk switch
 io.setup(desk_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
-#DESK = 1
 
 while True:
     if io.input(desk_pin):
         # The switch is open
-        print("Desk: Standing Position. Sending to Azure..."),
-        SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Standing" }')
-        print("done.")
+        print("Desk: Standing Position. Sending to Azure... ", end='')
+        try:
+            SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Standing" }')
+            print("done.")
+        except:
+            print("sending failed.")
 
-    if (io.input(desk_pin)==False):
+    if io.input(desk_pin)==False:
         # The switch is closed
-        print("Desk: Seated Position. Sending to Azure..."),
+        print("Desk: Seated Position. Sending to Azure... ", end='')
         SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Seated" }')
         print("done.")
 
-    time.sleep(10) # wait 10 seconds before the next action
+    time.sleep(10)
