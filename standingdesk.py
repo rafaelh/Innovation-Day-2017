b4f9a143ecf27089e90858bcf49c809458845488
Rafael Hart <rafael.hart9@gmail.com>
2017-05-04 17:42:54 +1000

    Formatting to PEP8 standards

--- standingdesk.py
+++ standingdesk.py
@@ -3,34 +3,37 @@ import sys
 from azure.servicebus import ServiceBusService
 import RPi.GPIO as io
 
-key_name="RootManageSharedAccessKey"
-key_value="pjxZvmffk+hhDTXST7TzvShuZ+jxgRiKLc1T4yLGOGw="
-
+DESK_PIN = 23
+KEY_NAME = "RootManageSharedAccessKey"
+KEY_VALUE = "pjxZvmffk+hhDTXST7TzvShuZ+jxgRiKLc1T4yLGOGw="
 SBS = ServiceBusService("stand-and-deliver",
-                        shared_access_key_name=key_name,
-                        shared_access_key_value=key_value)
+                        shared_access_key_name=KEY_NAME,
+                        shared_access_key_value=KEY_VALUE)
 
 # Set GPIO mode to BCM, using the GPIO number (in this case, 23)
 io.setmode(io.BCM)
-desk_pin = 23
 
-# Use the built-in PullUp resistor & initialize the desk switch
-io.setup(desk_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
+# Activate input with the built-in PullUp resistor
+io.setup(DESK_PIN, io.IN, pull_up_down=io.PUD_UP)
 
 while True:
-    if io.input(desk_pin):
+    if io.input(DESK_PIN):
         # The switch is open
         print("Desk: Standing Position. Sending to Azure... ", end='')
         try:
-            SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Standing" }')
+            SBS.send_event('stand-and-deliver',
+                           '{ "DeviceId": "StandingDesk1", "State": "Standing" }')
             print("done.")
         except:
             print("sending failed.")
 
-    if io.input(desk_pin)==False:
+    if io.input(DESK_PIN)==False:
         # The switch is closed
         print("Desk: Seated Position. Sending to Azure... ", end='')
-        SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Seated" }')
-        print("done.")
+        try:
+            SBS.send_event('stand-and-deliver', '{ "DeviceId": "StandingDesk1", "State": "Seated" }')
+            print("done.")
+        except:
+            print("sending failed.")
 
     time.sleep(10)
